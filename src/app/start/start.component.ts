1ff1b8d7f17e59b3248bb9d3a88f36e61337b70e
Michal <mazak.miso@gmail.com>
2020-06-22 15:48:47 +0200

    navigacia klavesnicou

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import * as Survey from 'survey-angular';
 import * as jsonFile from '../survey.json';
+import * as $ from "jquery";
 
 //Survey.StylesManager.applyTheme("default");
 
@@ -14,24 +15,60 @@ export class StartComponent implements OnInit {
 
   constructor() { }
 
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    var survey = (<any>window).survey;
+    var key = event.key;
+
+    if (survey != null) {
+      event.preventDefault();
+
+      if (key == "Enter" || key == "ArrowRight" || key == "ArrowDown") {
+        survey.nextPage();
+      } else if (key == "ArrowUp" || key == "ArrowLeft") {
+        survey.prevPage();
+      }
+    }
+  }
+
   ngOnInit(): void {
     var survey = new Survey.Model((jsonFile as any).default);
-    survey.onComplete.add(sendDataToServer);
+    survey.onComplete.add(onSurveyComplete);
+    survey.onCurrentPageChanged.add(insertAlternativeNextButton);
+    survey.onAfterRenderQuestion.add(doAfterRenderQuestion);
 
-    // survey.onUpdateQuestionCssClasses.add(function(survey, options) {
-    //   var classes = options.cssClasses;
+    function insertAlternativeNextButton() {
+      var $el = $(".sv_q_text_root");
+      if ($el.length > 0) {
+        $el = $($el[0]);
+        var value = $el.val();
+        var nextButtonAlreadyExists = $("#surveyNextAlternative").length > 0;
+        if (value != "" && !nextButtonAlreadyExists) {
+          $el.after('<input id="surveyNextAlternative" onclick="survey.nextPage();" type="button" value="Next" class="sv_next_btn sv_next_btn--alternative">')
+        } else if (value == "") {
+          $("#surveyNextAlternative").remove();
+        }
+      }
+    }
 
-    //   if (options.question.getType() === "matrix") {
-    //     classes.root = classes.root  + " custom-matrix";
-    //   }
-    // });
+    function doAfterRenderQuestion(survey) {
+      $(".sv_q_text_root").on("keyup", function (event) {
+        insertAlternativeNextButton();
+      });
+    }
 
     Survey.SurveyNG.render("surveyElement", { model: survey });
+    (<any>window).survey = survey;
+  }
+
+  manuallyCompleteSurvey() {
+    // document.getElementById("surveyContainer").hidden = true;
+    // document.getElementById("btnCompleteSurvey").hidden = true;
+    // document.getElementById("resultContainer").removeAttribute("hidden");
   }
 
 }
 
-function sendDataToServer(survey) {
-  //send (Ajax) request to your web server.
-  alert("The results are:" + JSON.stringify(survey.data));
+function onSurveyComplete(survey) {
+  
 }
